bc0afac13ca2c8fabdc4d216af7c75c2defebf65
pankajajoshi <joshi.pankaja@gmail.com>
2023-08-20 17:25:36 -0400

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require("express");
-const pokemon = require("./models/pokemon.js");
-
 const app = express();
 const port = 3000;
+
+const pokemon = require("./models/pokemon.js");
+
+require("@babel/register")({
+  presets: ["@babel/preset-react"],
+});
+
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Pokemon App!");
 });
 
 app.get("/pokemon", (req, res) => {
-  res.send(pokemon);
+  //res.send(pokemon);
+  res.render("Index", {
+    pokemon: pokemon,
+  });
 });
 
 app.listen(port, () => {
---
+++ views/Index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const myStyle = {
+  color: '#ffffff',
+  backgroundColor: '#000000',
+};
+
+class Index extends React.Component {
+  render() {
+    return (
+      <div style={myStyle}>
+        <h1>See All The Pokemon!</h1>
+      </div>
+    );
+  }
+}
+
+export default Index;
